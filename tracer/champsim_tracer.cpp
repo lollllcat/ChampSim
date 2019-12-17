472a08c67af85474d9a90911bf2d7daf5b4013e7
lollllcat <44036734+lollllcat@users.noreply.github.com>
2019-12-17 14:53:50 -0800

    Update champsim_tracer.cpp

--- tracer/champsim_tracer.cpp
+++ tracer/champsim_tracer.cpp
@@ -13,7 +13,7 @@
 
 #define NUM_INSTR_DESTINATIONS 2
 #define NUM_INSTR_SOURCES 4
-
+using namespace std;
 typedef struct trace_instr_format {
     unsigned long long int ip;  // instruction pointer (program counter) value
 
